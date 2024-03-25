d039f77c169072104c0bc2efde9a8fe92ef779a5
Christopher Hallett <chrisjameshal@gmail.com>
2024-03-25 21:23:58 +0000

    Basic CRUD operations

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 const express = require("express")
+const morgan = require("morgan")
+const mongoose = require("mongoose")
+const blogRoutes = require("./routes/blogRoutes")
 
 const app = express()
+require("dotenv").config()
 
-// listen for requests
-app.listen(3000)
+// connect to MongoDB
+const dbURI = `mongodb+srv://chrisjameshal:${process.env.MONGO_DB_PASSWORD}@blog-db.0o1fbw8.mongodb.net/node-blog?retryWrites=true&w=majority&appName=blog-db`
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(3000)) // only listen for requests if we have connected to the database
+  .catch((err) => console.log(err))
+
+// register view engine
+app.set("view engine", "ejs")
+// app.set("views", "myviews") // change the default views folder
+
+// Middleware & Static files
+app.use(express.static("public")) // serve static files
+app.use(morgan("dev")) // log requests to the console
+app.use(express.urlencoded({ extended: true })) // parse form data
 
 // Express looks through this code from top to bottom for matches
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname })
+  res.redirect("/blogs")
 })
 
+// Blog routes
+app.use("/blogs", blogRoutes)
+
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname })
+  res.render("about", { title: "About" })
 })
 
 // Redirect to /about
@@ -21,5 +41,5 @@ app.get("/about-me", (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname })
+  res.status(404).render("404", { title: "404" })
 })
